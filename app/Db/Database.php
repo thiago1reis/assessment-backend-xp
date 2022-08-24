71e4065735855c5430fd549f56aff871d60394fb
thiago1reis <thiagoalexandreis@gmail.com>
2022-08-24 18:02:21 -0300

    Estrutura de arquivos e diretorios

---
+++ app/Db/Database.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace App\DB;
+
+use \PDO;
+use PDOException;
+
+class Database{
+    // const HOST = 'localhost';
+    // const NAME = 'assessment_backend_xp';
+    // const USER = 'root';
+    // const PASS = '';
+    private $table; 
+    private $connection;
+
+    public function __construct($table = null){
+        $this->table = $table;
+        $this->seteConnection();
+    }
+
+    private function seteConnection(){
+        try{
+            $this->connection = new PDO("mysql:host=localhost;dbname=assessment_backend_xp", "root", "");
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            die('ERROR: '.$e->getMessage());
+        }
+    }
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'assessment-backend-xp/desafio',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f3a267ddb71ddd5b811f27869649bec46885e222',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'assessment-backend-xp/desafio' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f3a267ddb71ddd5b811f27869649bec46885e222',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
